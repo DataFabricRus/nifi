fa56b22bb040d666488fb0a3d3d80c8424eebd4d
DramaticallyDecayed <lebedev.sv.etu@gmail.com>
2017-12-28 10:41:03 +0300

    Redesign CreateGCDataflowJobFromTeplate.java
    Redesign said class so that to satisfy NiFi concept of FlowFile circulation. Instead of storing info about job in process FlowFiles are created with job state. These flowfiles can be updated with the help of dataflow "get job" request.

--- nifi-nar-bundles/nifi-gcp-bundle/nifi-gcp-processors/src/main/java/org/apache/nifi/processors/gcp/dataflow/CreateGCDataflowJobFromTeplate.java
+++ nifi-nar-bundles/nifi-gcp-bundle/nifi-gcp-processors/src/main/java/org/apache/nifi/processors/gcp/dataflow/CreateGCDataflowJobFromTeplate.java
@@ -39,10 +39,6 @@ import java.util.*;
 public class CreateGCDataflowJobFromTeplate extends AbstractProcessor {
     //TODO: think about the name. It it can be called an Ingress Processor then possibly it would be better to start name from Listen. E.g. ListenGCDJobREST
 
-    private Map<String, List<String>> ids = new HashMap<>();
-
-
-    // Identifies the distributed map cache client
     public static final PropertyDescriptor DATAFLOW_SERVICE = new PropertyDescriptor.Builder()
             .name("dataflow-service-provider")
             .displayName("Dataflow Service Provider")
@@ -96,6 +92,11 @@ public class CreateGCDataflowJobFromTeplate extends AbstractProcessor {
             .defaultValue("europe-west1")
             .build();
 
+    public static final String JOB_ID_ATTR = "job.id";
+    public static final String JOB_NAME_ATTR = "job.name";
+    private static final String JOB_VALIDATE_ATTR = "job.validate";
+    private static final String JOB_TEMPLATE_PATH_ATTR = "job.template.path";
+
 
     @Override
     public List<PropertyDescriptor> getSupportedPropertyDescriptors() {
@@ -129,6 +130,11 @@ public class CreateGCDataflowJobFromTeplate extends AbstractProcessor {
                     .description("FlowFiles are routed to this relationship after a failure on Google Cloud Dataflow launch operation.")
                     .build();
 
+    public static final Relationship REL_INPROCESS =
+            new Relationship.Builder().name("inprocess")
+                    .description("FlowFiles with job id.")
+                    .build();
+
     public static final Relationship REL_NOTIFY =
             new Relationship.Builder().name("notify")
                     .description("FlowFiles with job status information are routed to this relationship while Google Cloud Dataflow launch operation.")
@@ -147,6 +153,7 @@ public class CreateGCDataflowJobFromTeplate extends AbstractProcessor {
         final Set<Relationship> relationships = new HashSet<>();
         relationships.add(REL_SUCCESS);
         relationships.add(REL_FAILURE);
+        relationships.add(REL_INPROCESS);
         relationships.add(REL_NOTIFY);
         this.relationships = Collections.unmodifiableSet(relationships);
 
@@ -154,29 +161,28 @@ public class CreateGCDataflowJobFromTeplate extends AbstractProcessor {
 
     @Override
     public void onTrigger(ProcessContext context, ProcessSession session) throws ProcessException {
-        FlowFile flowFile = null;
+        FlowFile flowFile = session.get();
         try {
             Dataflow dataflowService = context
                     .getProperty(DATAFLOW_SERVICE)
                     .asControllerService(GCPDataflowService.class)
                     .getDataflowService();
 
-            flowFile = session.get();
-
-
             if (flowFile == null) {
-                examineJobs(context, session, dataflowService);
+                return;
             } else {
-                launchJob(dataflowService, context, session, flowFile);
-                examineJobs(context, session, dataflowService);
+                String jobId = flowFile.getAttribute(JOB_ID_ATTR);
+                if (jobId == null) {
+                    launchJob(dataflowService, context, session, flowFile);
+                } else if (!jobId.isEmpty()) {
+                    examineJobs(context, session, dataflowService, flowFile);
+                } else {
+                    getLogger().error("Flowfile has id job.id field but it is empty!");
+                    session.transfer(flowFile, REL_FAILURE);
+                }
             }
-
         } catch (final Exception e) {
             getLogger().error("Failed to launch job due to ", e);
-            if (flowFile != null) {
-                flowFile = session.penalize(flowFile);
-                session.transfer(flowFile, REL_FAILURE);
-            }
         }
     }
 
@@ -186,8 +192,6 @@ public class CreateGCDataflowJobFromTeplate extends AbstractProcessor {
             ProcessSession session,
             FlowFile flowFile
     ) throws IOException {
-
-
         Map<String, String> parameters = new HashMap<>();
         for (final Map.Entry<PropertyDescriptor, String> entry : context.getProperties().entrySet()) {
             if (entry.getKey().isDynamic()) {
@@ -206,8 +210,8 @@ public class CreateGCDataflowJobFromTeplate extends AbstractProcessor {
                 .getValue();
 
         launchTemplateParameters.setJobName(jobName);
-        launchTemplateParameters.setParameters(parameters);
 
+        launchTemplateParameters.setParameters(parameters);
 
         String projectId = context.getProperty(PROJECT_ID)
                 .evaluateAttributeExpressions(flowFile)
@@ -232,14 +236,18 @@ public class CreateGCDataflowJobFromTeplate extends AbstractProcessor {
 
         if (!launch.getValidateOnly()) {
             if (launchResponse != null && launchResponse.getJob() != null && launchResponse.getJob().getId() != null) {
-                List<String> jobParameters = new ArrayList<>();
-                jobParameters.add(projectId);
-                jobParameters.add(gcpPath);
-                jobParameters.add(jobName);
-                ids.put(launchResponse.getJob().getId(), jobParameters);
+                session.transfer(
+                        buildFlowFile(context, session, flowFile, launchResponse.getJob().getId()),
+                        REL_INPROCESS
+                );
             } else {
                 throw new ProcessException("Get null while job launching");
             }
+        } else {
+            session.transfer(
+                    buildFlowFile(context, session, flowFile, ""),
+                    REL_SUCCESS
+            );
         }
         notifyLaunchPreparation(context, session, parameters, flowFile);
     }
@@ -249,7 +257,10 @@ public class CreateGCDataflowJobFromTeplate extends AbstractProcessor {
             ProcessSession session,
             Map<String, String> parameters,
             FlowFile flowFile) {
+
+        FlowFile notification = null;
         try {
+            notification = session.create();
             Map<String, String> attributes = new HashMap<>();
             List<Field> fields = new ArrayList<>();
             fields.add(new Field("job name", context.getProperty(JOB_NAME).evaluateAttributeExpressions(flowFile).getValue()));
@@ -268,28 +279,30 @@ public class CreateGCDataflowJobFromTeplate extends AbstractProcessor {
 
             attributes.put("attachments", (new ObjectMapper()).writeValueAsString(message));
             if (attributes.size() > 0) {
-                flowFile = session.putAllAttributes(flowFile, attributes);
+                notification = session.putAllAttributes(notification, attributes);
             }
-            session.transfer(flowFile, REL_NOTIFY);
+            session.transfer(notification, REL_NOTIFY);
 
         } catch (Exception e) {
             getLogger().error("Can't create notification!", e);
-            flowFile = session.penalize(flowFile);
-            session.transfer(flowFile, REL_FAILURE);
+            if (notification != null) {
+                session.remove(notification);
+            }
         }
     }
 
     private void notifyJobState(
             ProcessSession session,
-            String id,
-            String state) {
-        FlowFile flowFile = null;
+            String state,
+            FlowFile flowFile
+    ) {
+        FlowFile notification = null;
         try {
-            flowFile = session.create();
+            notification = session.create();
             Map<String, String> attributes = new HashMap<>();
             List<Field> fields = new ArrayList<>();
-            List<String> jobParameters = ids.get(id);
-            fields.add(new Field("job name", jobParameters.get(2)));
+            String id = flowFile.getAttribute(JOB_ID_ATTR);
+            fields.add(new Field("job name", flowFile.getAttribute(JOB_NAME_ATTR)));
             fields.add(new Field("job id", id));
             fields.add(new Field("job state", state));
             Message message = new Message();
@@ -319,82 +332,78 @@ public class CreateGCDataflowJobFromTeplate extends AbstractProcessor {
             message.setFields(fields);
             attributes.put("attachments", (new ObjectMapper()).writeValueAsString(message));
             if (attributes.size() > 0) {
-                flowFile = session.putAllAttributes(flowFile, attributes);
+                notification = session.putAllAttributes(notification, attributes);
             }
-            session.transfer(flowFile, REL_NOTIFY);
+            session.transfer(notification, REL_NOTIFY);
         } catch (Exception e) {
             getLogger().error("Can't create notification!", e);
-            if (flowFile != null) {
-                session.transfer(flowFile, REL_FAILURE);
-            }
+            session.remove(notification);
         }
     }
 
     private void examineJobs(
             ProcessContext context,
             ProcessSession session,
-            Dataflow dataflowService
+            Dataflow dataflowService,
+            FlowFile flowFile
     ) throws IOException {
-        Iterator<String> iterator = ids.keySet().iterator();
-        while (iterator.hasNext()) {
-            String id = iterator.next();
-
-            List<String> jobParameters = ids.get(id);
-
-            Dataflow.Projects.Locations.Jobs.Get request = dataflowService.projects().locations().jobs().get(
-                    jobParameters.get(0),
-                    context.getProperty(LOCATION).getValue(),
-                    id
-            );
-
-            Job response = request.execute();
 
 
-            String state = response.getCurrentState();
+        String id = flowFile.getAttribute(JOB_ID_ATTR);
 
-            if (state == null) {
-                //TODO: count attempts?
-                continue;
+        Dataflow.Projects.Locations.Jobs.Get request = dataflowService.projects().locations().jobs().get(
+                context.getProperty(PROJECT_ID)
+                        .evaluateAttributeExpressions(flowFile)
+                        .getValue(),
+                context.getProperty(LOCATION).getValue(),
+                id
+        );
+
+        Job response = request.execute();
+
+        String state = response.getCurrentState();
+
+        if (state == null) {
+            //TODO: count attempts?
+            session.transfer(flowFile, REL_INPROCESS);
+            return;
+        }
+
+        switch (state) {
+            case "JOB_STATE_RUNNING":
+            case "JOB_STATE_PENDING":
+            case "JOB_STATE_CANCELLING":
+                //notifyJobState(session, state, flowFile);
+                session.transfer(flowFile, REL_INPROCESS);
+                break;
+            case "JOB_STATE_DONE": {
+                notifyJobState(session, state, flowFile);
+                buildFlowFile(context, session, flowFile, id);
+                session.getProvenanceReporter().create(flowFile, "It takes something about"
+                        + response.getCurrentStateTime()
+                        + " to perform the job");
+                getLogger().info("The job with id " + id + " has been done!");
+                session.transfer(flowFile, REL_SUCCESS);
+                break;
             }
-
-            switch (state) {
-                case "JOB_STATE_RUNNING":
-                case "JOB_STATE_PENDING":
-                case "JOB_STATE_CANCELLING":
-                    notifyJobState(session, id, state);
-                    break;
-                case "JOB_STATE_DONE": {
-                    notifyJobState(session, id, state);
-                    FlowFile flowFile = session.create();
-                    buildFlowFile(context, session, flowFile, id);
-                    session.getProvenanceReporter().create(flowFile, "It takes something about"
-                            + response.getCurrentStateTime()
-                            + " to perform the job");
-                    session.transfer(flowFile, REL_SUCCESS);
-                    getLogger().info("The job with id " + id + " has been done!");
-                    iterator.remove();
-                    break;
-                }
-                case "JOB_STATE_UNKNOWN":
-                case "JOB_STATE_FAILED":
-                case "JOB_STATE_CANCELLED": {
-                    notifyJobState(session, id, state);
-                    FlowFile flowFile = session.create();
-                    buildFlowFile(context, session, flowFile, id);
-                    session.transfer(flowFile, REL_FAILURE);
-                    getLogger().info("The job with id {} unsuccessfully terminated with the state {}!", new Object[]{id, state});
-                    iterator.remove();
-                    break;
-                }
-                case "JOB_STATE_STOPPED":
-                    notifyJobState(session, id, state);
-                    getLogger().info("The job with id " + id + " has been stopped!");
-                    break;
-                default:
-                    notifyJobState(session, id, state);
-                    getLogger().error("The job with id {} is in unworkable {} state!", new Object[]{id, state});
-                    iterator.remove();
+            case "JOB_STATE_UNKNOWN":
+            case "JOB_STATE_FAILED":
+            case "JOB_STATE_CANCELLED": {
+                notifyJobState(session, state, flowFile);
+                buildFlowFile(context, session, flowFile, id);
+                getLogger().info("The job with id {} unsuccessfully terminated with the state {}!", new Object[]{id, state});
+                session.transfer(flowFile, REL_FAILURE);
+                break;
             }
+            case "JOB_STATE_STOPPED":
+                notifyJobState(session, state, flowFile);
+                getLogger().info("The job with id " + id + " has been stopped!");
+                session.transfer(flowFile, REL_INPROCESS);
+                break;
+            default:
+                notifyJobState(session, state, flowFile);
+                getLogger().error("The job with id {} is in unworkable {} state!", new Object[]{id, state});
+                session.transfer(flowFile, REL_FAILURE);
         }
     }
 
@@ -405,14 +414,29 @@ public class CreateGCDataflowJobFromTeplate extends AbstractProcessor {
             FlowFile flowFile,
             String id
     ) {
-        List<String> jobParameters = ids.get(id);
         Map<String, String> attributes = new HashMap<>();
-        attributes.put(JOB_NAME.getName(), jobParameters.get(2));
-        attributes.put(VALIDATE_ONLY.getName(), context.getProperty(VALIDATE_ONLY).getValue());
-        attributes.put(GCS_PATH.getName(), jobParameters.get(1));
+
+
+        attributes.put(JOB_ID_ATTR, id);
+
+        attributes.put(
+                JOB_NAME_ATTR,
+                context.getProperty(JOB_NAME).evaluateAttributeExpressions(flowFile).getValue()
+        );
+        attributes.put(
+                JOB_VALIDATE_ATTR,
+                context.getProperty(VALIDATE_ONLY).getValue()
+        );
+        attributes.put(
+                JOB_TEMPLATE_PATH_ATTR,
+                context.getProperty(GCS_PATH).evaluateAttributeExpressions(flowFile).getValue()
+        );
+
+
         if (attributes.size() > 0) {
             flowFile = session.putAllAttributes(flowFile, attributes);
         }
+
         return flowFile;
     }
 
